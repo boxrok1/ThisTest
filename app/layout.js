abfdf61427bad6062dc623c91b89caf476f81b7c
boxrok1 <box404update@gmail.com>
2026-03-21 02:39:57 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "My Form App",
+  description: "Simple Next.js form",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
